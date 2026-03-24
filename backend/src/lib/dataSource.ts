d2678dd1585bca16a71e697f43e93ace7836f49c
LoicBrassart <loic.brassart@gmail.com>
2026-03-24 21:41:28 +0100

    Use data migrations everywhere except dev

--- backend/src/lib/dataSource.ts
+++ backend/src/lib/dataSource.ts
@@ -4,12 +4,18 @@ import { configDotenv } from "dotenv";
 
 configDotenv();
 const { env } = process;
-if (!env.DB_HOST || !env.DB_PASSWORD || !env.DB_DATABASE || !env.DB_USER)
+if (
+  !env.DB_HOST ||
+  !env.DB_PASSWORD ||
+  !env.DB_DATABASE ||
+  !env.DB_USER ||
+  !env.ENV
+)
   throw new Error("Missing env variables!");
 
 export const dataSource = new DataSource({
   entities,
-  synchronize: true,
+  synchronize: env.ENV === "dev",
   logging: false,
 
   type: "postgres",
